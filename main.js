2cddd591d42f4e60f11cbcba56e21562cd99e2ee
Mission Marcus <missionmarcus@gmail.com>
2018-12-27 15:30:55 -0600

    Added all files.
    Added starter files.

---
+++ main.js
@@ -0,0 +1,36 @@
+﻿// Your code here!
+
+
+var globalID;
+
+
+
+
+
+function changePosition() {
+
+    var title = document.getElementsByClassName("Nameheader");
+
+    var pos = 500;
+
+    var timeStamp = 0;
+
+    function moveIt(timestamp) {
+
+        if (pos <= -100) {
+            timeStamp = cancelAnimationFrame(timeStamp);
+            changeColor();
+        }
+        else {
+
+            pos = pos - 4;
+            title.style.marginLeft = pos + 'px';
+            requestAnimationFrame(moveIt);
+        }
+    }
+
+    requestAnimationFrame(moveIt);
+
+
+}
+
